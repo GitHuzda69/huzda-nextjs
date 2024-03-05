807348f48562925d5dcb6c001d88e273c137ebc4
GitHuzda69 <hprmaa19@gmail.com>
2024-03-05 17:12:16 +0700

    fix route before deploy

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,13 +1,14 @@
 import styles from "@/styles/Sidebar.module.css"
+import Link from "next/link";
 
 export default function Sidebar () {
     return (
         <div className={styles.sidebar}>
       <h2>Sidebar</h2>
       <ul>
-        <a href="/dashboard">Dashboard</a>
+        <Link href="/dashboard">Dashboard</Link>
         <li>Menu Item 2</li>
-        <a href="/about">About</a>
+        <Link href="/about">About</Link>
       </ul>
     </div>
     )
